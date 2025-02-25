07372af5e4fc2bf707d7bc456347665ce8e491a0
Niiko Jr <nalas907@gmail.com>
2025-02-25 04:04:21 -0600

    Implementación de la clase "Boleto"; validación de tipos y precios de boletos, dependiendo del tipo de boleto(INFANTIL, ADULTO, VIP), se asigna un precio diferente.

--- src/main/java/park/Boleto.java
+++ src/main/java/park/Boleto.java
@@ -1,4 +1,35 @@
 package park;
 
 public class Boleto {
+
+    //enumeracion tipos de boletos
+    public enum Tipo { INFANTIL, ADULTO, VIP }
+
+    private Tipo tipo;
+    private double precio;
+
+    public Boleto(Tipo tipo) {
+        this.tipo = tipo;
+        switch (tipo) {
+            case INFANTIL:
+                this.precio = 5.0;
+                break;
+            case ADULTO:
+                this.precio = 10.0;
+                break;
+            case VIP:
+                this.precio = 20.0;
+                break;
+            }
+        }
+
+        public double getPrecio() {
+        return precio;
+    }
+
+        public Tipo getTipo() {
+        return tipo;
+    }
+
 }
+
