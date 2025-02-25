87cbb5c1c49bdd3b941108d9f4788e67ffc040d8
Niiko Jr <nalas907@gmail.com>
2025-02-25 04:14:23 -0600

    Implementación de la clase "Visitante" y validación (si el visitante tiene un boleto VIP).

--- src/main/java/park/Visitante.java
+++ src/main/java/park/Visitante.java
@@ -1,4 +1,43 @@
 package park;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Visitante {
+
+    private String nombre;
+    private int edad;
+    private List<Boleto> listaDeBoletos;
+
+    public Visitante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.listaDeBoletos = new ArrayList<Boleto>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void agregarBoleto(Boleto boleto) {
+        listaDeBoletos.add(boleto);
+    }
+
+
+    //validaciones
+    public boolean tieneBoletoVIP() {
+        for (Boleto boleto : listaDeBoletos) {
+            if (boleto.getTipo() == Boleto.Tipo.VIP) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Boleto>getListaDeBoletos(){
+        return listaDeBoletos;
+    }
 }
