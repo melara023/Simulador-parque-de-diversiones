7918e043ff10cbfb5b7473620842c43b5c9a0db8
Niiko Jr <nalas907@gmail.com>
2025-02-25 02:46:04 -0600

    Implementación de la super clase "Atraccion"; validación de "Edad Mínima" y "Capacidad Máxima", validación de "Boleto VIP"(si el visitante tiene un boleto VIP, no se verifica la edad mínima).

--- src/main/java/park/Atraccion.java
+++ src/main/java/park/Atraccion.java
@@ -1,4 +1,92 @@
 package park;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Atraccion {
-}
+    interface Operable {
+        void iniciar();
+
+        void detener();
+
+        void realizarMantenimiento();
+
+    }
+
+
+    public abstract class Atraccion {
+        protected String nombre;
+        protected int capacidadMaxima;
+        protected int edadMinima;
+        protected boolean enMantenimiento;
+        protected List<Visitante> visitantes;
+
+
+    public Atraccion(String nombre, int capacidadMaxima, int edadMinima) {
+        this.nombre = nombre;
+        this.capacidadMaxima = capacidadMaxima;
+        this.edadMinima = edadMinima;
+        this.enMantenimiento = false;
+        this.visitantes = new ArrayList<Visitante>();
+    }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public int getCapacidadMaxima() {
+            return capacidadMaxima;
+        }
+
+        public int getEdadMinima() {
+            return edadMinima;
+        }
+
+        public boolean isEnMantenimiento() {
+            return enMantenimiento;
+        }
+
+        //validaciones
+        public boolean agregarVisitante(Visitante visitante) {
+            if (visitantes.size() < capacidadMaxima) { //validacion capacidad maxima
+                if (visitante.tieneBoletoVIP() || visitante.getEdad() >= edadMinima) { //validacion de edad minima o VIP
+                    visitantes.add(visitante);
+                    return true;
+                } else {
+                    System.out.println(visitante.getNombre() + " no cumple con la edad mínima para " + nombre);
+                }
+            } else {
+                System.out.println(nombre + " está lleno.");
+            }
+            return false;
+        }
+
+        //metodo reubicar a otra atraccion
+        public void reubicarVisitantes(Atraccion nuevaAtraccion) {
+            for (Visitante visitante : new ArrayList<Visitante>(visitantes)) {
+                if (nuevaAtraccion.agregarVisitante(visitante)) {
+                    System.out.println(visitante.getNombre() + " ha sido reubicado a la atracción: " + nuevaAtraccion.getNombre());
+                    visitantes.remove(visitante);
+                }
+            }
+        }
+
+        @Override
+        public void iniciar() {
+            if (!enMantenimiento) {
+                System.out.println(nombre + " ha comenzado a funcionar.");
+            } else {
+                System.out.println(nombre + " no puede iniciar porque está en mantenimiento.");
+            }
+        }
+        @Override
+        public void detener() {
+            System.out.println(nombre + " se ha detenido.");
+        }
+
+        @Override
+        public void realizarMantenimiento() {
+            enMantenimiento = true;
+            System.out.println(nombre + " está en mantenimiento.");
+        }
+
+    }
+
